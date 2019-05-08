896a0b038220adc3af4457d9f01603bb567f3de1
MahmoudAlzoghbi <mahmodalzoghby111@gmail.com>
2019-05-08 04:34:32 +0200

    third commit

---
+++ app/src/main/java/com/example/mahmoudalzoghby/graduationproject/ClientSeeWorkerProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.mahmoudalzoghby.graduationproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientSeeWorkerProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_see_worker_profile);
+    }
+}
