07cd842dfdf79ef98cf85c852bc55a1b0102c170
MahmoudAlzoghbi <mahmodalzoghby111@gmail.com>
2019-06-10 13:05:27 +0200

    six commit

---
+++ app/src/main/java/com/example/mahmoudalzoghby/graduationproject/ShowGovActivity.java
@@ -0,0 +1,13 @@
+package com.example.mahmoudalzoghby.graduationproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowGovActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_gov);
+    }
+}
